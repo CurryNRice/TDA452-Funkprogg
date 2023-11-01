6dc5398768ac1e2a411faf863a4aad86cc0e9d4c
Karl Kjellmer <karl.kjellmer@gmail.com>
2023-11-01 11:47:50 +0100

    Finich lab 1, add lab 2

--- Lab1/Lab1.hs
+++ Lab1/Lab1.hs
@@ -1,9 +1,9 @@
 {- Lab 1
-   Date: 
-   Authors:
-   Lab group:
+   Date: 2023-11-01
+   Authors: Karl Kjellmer, Albin Skeppstedt
+   Lab group: 5
  -}
- import Test.QuickCheck
+import Test.QuickCheck
 
 --------------------------------------------
 power :: Integer -> Integer -> Integer
@@ -17,46 +17,43 @@ power n k  = n * power n (k-1)
 -- power n k takes to compute
 
 stepsPower :: Integer -> Integer -> Integer
-
 stepsPower n k = k+1 
 
 
 -- B -------------------------
 power1 :: Integer -> Integer -> Integer
-
 power1 n k = product(replicate (fromInteger k) n)
 
 -- C -------------------------
 power2 :: Integer -> Integer -> Integer
-
 power2 n 0 = 1
-
-{- power2 n k = if even k then power2 (n*n) (div k 2) else n * power2 n (k-1) -}
-
 power2 n k | even k    = power2 (n*n) (div k 2)
            | otherwise = n * power2 n (k-1)
 
 -- D -------------------------
 {- 
-
 <Describe your test cases here>
 
-5^1
-2^0
-2^2
-
-
+5^1 - Testing the identity
+2^0 - Testing the special case ^0
+2^2 - Testig a generic case 
+This tests "all" interesting cases of non negative numbers
  -}
 
 -- 
 prop_powers :: Integer -> Integer -> Bool
-prop_powers n k = and [(power n k == power1 n k), (power n k == power2 n k)]
+prop_powers n k = and [(pow == power1 n k), (pow == power2 n k)]
+   where pow = power n k 
+
 
 --
 powerTest :: Bool
-powerTest = undefined
+powerTest = and [prop_powers (fst x) (snd x) | x <- tests]
+   where tests = [(5, 1), (2, 0), (2, 2)]
 
 --
-prop_powers' n k = and [(power n' k' == power1 n' k'), (power n' k' == power2 n' k')]
+prop_powers' :: Integer -> Integer -> Bool
+prop_powers' n k = and [(pow == power1 n' k'), (pow == power2 n' k')]
    where n' = abs n
          k' = abs k
+         pow = power n' k'
