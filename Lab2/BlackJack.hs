3a867c4e800bbb70687b67f93ff067ec94fca88b
SkepparAbbe <albske12@gmail.com>
2023-11-09 15:34:46 +0100

    Finished lab2B

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -64,7 +64,7 @@ displayCard (Card r c) = show r ++ " of " ++ show c
 --     to the rules of Blackjack.
 value :: Hand -> Integer
 value Empty = 0
-value hand = if val > 21 then (val - 10*numberOfAces hand) else val 
+value hand = if val > 21 then val - 10*numberOfAces hand else val 
   where 
     val = value' hand 0
 
@@ -83,7 +83,7 @@ value' (Add _ h) v                    = value' h (10+v)
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add (Card r c) h) 
-              | r == Ace = 1 + numberOfAces h
+              | r == Ace  = 1 + numberOfAces h
               | otherwise = numberOfAces h
 
 
@@ -108,7 +108,7 @@ winner h1 h2 | gameOver h1         = Bank
 
 -- B1: Given two hands, <+ puts the first one on top of the second one:
 (<+) :: Hand -> Hand -> Hand
-Empty <+ hy = hy
+Empty <+ hy     = hy
 (Add c h) <+ hy = Add c (h <+ hy)
 
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool 
@@ -120,21 +120,16 @@ prop_size_onTopOf h1 h2 = size h1 + size h2 == size (h1 <+ h2)
 ------------------------------------------------------------
 
 -- B2: You also need to define a function that returns a full deck of cards
-ranks = [Numeric 2, Numeric 3, Numeric 4, Numeric 5, Numeric 6, Numeric 7,
-         Numeric 8, Numeric 9, Numeric 10, Jack, Queen, King, Ace]
-suits = [Hearts, Spades, Diamonds, Clubs]
-fullDeck :: Hand
-fullDeck = fromListToHand [Card r s | s <- suits, r <- ranks]
-
-fromListToHand :: [Card] -> Hand
-fromListToHand [] = Empty
-fromListToHand (c:cd) = Add c (fromListToHand cd)  
+fullDeck :: Hand 
+fullDeck = foldr (<+) Empty [Add (Card (Numeric n) s) Empty | n <- [2..10],        s <- suits] <+
+           foldr (<+) Empty [Add (Card r s) Empty | r <- [Ace, Jack, Queen, King], s <- suits]
+           where suits = [Diamonds, Spades, Hearts, Clubs]
 ------------------------------------------------------------
 
 -- B3: Given a deck and a hand, draw one card from the deck and put on the hand. Return both the deck and the hand (in that order). 
 draw :: Hand -> Hand -> (Hand,Hand)
-draw Empty h = error "draw: The deck is empty."
-draw (Add c rd) h = (rd, (Add c h))
+draw Empty h      = error "draw: The deck is empty."
+draw (Add c rd) h = (rd, Add c h)
 
 -- If the deck is empty, report an error using error:
 -- error "draw: The deck is empty."
@@ -157,33 +152,19 @@ playBankHelper deck hand = if value biggerHand >= 16 then biggerHand else playBa
 -- (but note that random number generation is not what makes this a harder assignment).
 
 -- Given a StdGen and a hand of cards, shuffle the cards and return the shuffled hand:
-
-
 shuffleDeck :: StdGen -> Hand -> Hand
 shuffleDeck _ Empty = Empty
 shuffleDeck g hand=  Add card (shuffleDeck g' restHand)
             where                    
                   (x, g') = randomR (0, size hand - 1) g
-                  (card, restHand) = takeCardByIndex x hand
+                  (card, restHand) = takeCardByIndex Empty hand x
+                  takeCardByIndex :: Hand -> Hand -> Int -> (Card, Hand)
+                  takeCardByIndex checked (Add c rest) 0 = (c, checked <+ rest)
+                  takeCardByIndex checked (Add c rest) n = removeNthCardHelper (Add c checked) rest (n-1) 
 
-takeCardByIndex :: Int -> Hand -> (Card, Hand)
-takeCardByIndex n h = (card, (hHead <+ hTail))
-          where 
-            (Add card hTail) = dropCards n h 
-            hHead = takeCards n h
-
-takeCards :: Int -> Hand -> Hand
-takeCards 0 _ = Empty
-takeCards 1 (Add c h) = (Add c Empty)
-takeCards n (Add c h) = Add c (takeCards (n-1) h)
-
-dropCards :: Int -> Hand -> Hand 
-dropCards 0 h = h
-dropCards n (Add c h) = dropCards (n-1) h
-
+--Property for shuffleDeck, if a deck has been shuffled, the shuffled deck contains the same cards.
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool 
 prop_shuffle_sameCards g c h = c `belongsTo` h == c `belongsTo` shuffleDeck g h
-
-belongsTo :: Card -> Hand -> Bool 
-c `belongsTo` Empty = False 
-c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
+        where  belongsTo :: Card -> Hand -> Bool 
+               c `belongsTo` Empty      = False 
+               c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
