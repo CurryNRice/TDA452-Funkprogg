4e234240b183d9f4f36b748caa314a5621c6ef14
Karl Kjellmer <karl.kjellmer@gmail.com>
2023-11-24 20:32:47 +0000

    heheheh

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -3,14 +3,15 @@ module Sudoku where
 import Test.QuickCheck
 --import Data.List(replicate)
 import Data.Char(isDigit, digitToInt)
-import Data.List(nub)
+import Data.List
+import Data.Maybe(isJust)
 
 ------------------------------------------------------------------------------
 
 -- Ä1
 -- | Representation of a Digit type that makes the implementation safer
 --TODO 
---data Digit =
+--data Digit = 
 
 -- | Representation of sudoku puzzles (allows some junk)
 type Cell = Maybe Int -- a single cell
@@ -40,21 +41,6 @@ example =
     n = Nothing
     j = Just
 
-example2 :: Sudoku
-example2 =
-    Sudoku
-      [ [j 3,j 3,n  ,n  ,j 7,j 1,j 2,n  ,n  ]
-      , [n  ,j 5,n  ,n  ,n  ,n  ,j 1,j 8,n  ]
-      , [n  ,n  ,j 9,j 2,n  ,j 4,j 7,n  ,n  ]
-      , [n  ,n  ,n  ,n  ,j 1,j 3,n  ,j 2,j 8]
-      , [j 4,n  ,n  ,j 5,n  ,j 2,n  ,n  ,j 9]
-      , [j 2,j 7,n  ,j 4,j 6,n  ,n  ,n  ,n  ]
-      , [n  ,n  ,j 5,j 3,n  ,j 8,j 9,n  ,n  ]
-      , [n  ,j 8,j 3,n  ,n  ,n  ,n  ,j 6,n  ]
-      ]
-  where
-    n = Nothing
-    j = Just
 
 -- * A1
 
@@ -66,35 +52,31 @@ allFilledSudoku :: Sudoku
 allFilledSudoku = Sudoku (replicate 9 (replicate 9 $ Just 1))
 -- * A2
 
--- | isSudoku sud checks if sud is really a valid representation of a sudoku, i.e. is there 9 elements in the sudoku and are all of those nine elements long.
--- puzzle
+-- | isSudoku sud checks if sud is really a valid representation of a sudoku,
+--   i.e. is there 9 elements in the sudoku and are all of those nine elements long.
 isSudoku :: Sudoku -> Bool
-isSudoku s = ((length $ rows s) == 9) 
-           && checkRowsLength s                                      
-           && checkValidEntries s checkElement
+isSudoku s = (length (rows s) == 9)
+           && checkRowsLengths s                                      
+           && checkValidEntries s checkCell
            where 
-                checkRowsLength   :: Sudoku -> Bool
-                checkRowsLength   s = and (map (\n -> n == 9) (map length (rows s)))
-checkElement :: Maybe Int -> Bool
-checkElement e = case e of
-          Just e  -> (e <= 9 && e >= 0)
+                checkRowsLengths s = all ((== 9) . length) (rows s)
+
+-- | Checks whether a cell is a valid cell
+checkCell :: Maybe Int -> Bool
+checkCell e = case e of
+          Just e  -> e <= 9 && e > 0
           Nothing -> True
 
+-- | Checks whether all cells in a sudoku satesfies the function applied
 checkValidEntries :: Sudoku -> (Maybe Int -> Bool) -> Bool
-checkValidEntries s func = and (map rowCheck (rows s))
-                where
-                    rowCheck :: Row -> Bool
-                    rowCheck r  = and (map func r)
+checkValidEntries s func = all (all func) (rows s)
 -- * A3
 
 -- | isFilled sud checks if sud is completely filled in,
 -- i.e. there are no blanks
 isFilled :: Sudoku -> Bool
-isFilled s = checkValidEntries s checkElement
-          where
-            checkElement e = case e of
-                             Just e -> True
-                             Nothing -> False
+isFilled s = checkValidEntries s (isJust)
+
 
 ------------------------------------------------------------------------------
 
@@ -102,18 +84,10 @@ isFilled s = checkValidEntries s checkElement
 
 -- | printSudoku sud prints a nice representation of the sudoku sud on
 -- the screen
-
---Should change all elements to n or ., then print each row, then 
 printSudoku :: Sudoku -> IO ()
-printSudoku s = putStr (combineRows (map createChars (rows s)))
+printSudoku s = putStr (unlines (map (map maybeToChar) (rows s)))
             where
-              combineRows :: [[Char]] -> String
-              combineRows (r:rs) = r ++ ['\n'] ++ combineRows rs
-              combineRows []     = []
-              createChars :: [Maybe Int] -> [Char]
-              createChars r = map monadToChar r
-              monadToChar :: Maybe Int -> Char
-              monadToChar n = case n of
+              maybeToChar n = case n of
                               Just n -> head (show n)
                               Nothing -> '.'
 
@@ -126,11 +100,12 @@ readSudoku filePath =
                     do
                       s <- readFile filePath
                       let sudoku = parseSudokuFile (lines s)
-                      if (isSudoku sudoku)
+                      if isSudoku sudoku
                         then return sudoku
-                      else error("file does not contain a valid Sudoku")
+                      else error "file does not contain a valid Sudoku"
                       return sudoku
 
+-- | parseSudokuFile takes the rows of a Sudoku string, and parses it to the row type and builds a Sudoku from it.
 parseSudokuFile :: [String] -> Sudoku
 parseSudokuFile s = Sudoku $ map buildRow s
               where
@@ -138,10 +113,10 @@ parseSudokuFile s = Sudoku $ map buildRow s
                 buildRow :: [Char] -> [Cell]
                 buildRow s = buildRowHelper s []
                 buildRowHelper :: String -> [Cell] -> [Cell]
-                buildRowHelper (s:ss) l | isDigit s = buildRowHelper ss $ (Just (digitToInt s)):l
-                                        | s == '.'  = buildRowHelper ss $ (Nothing):l
+                buildRowHelper (s:ss) l | isDigit s = buildRowHelper ss (Just (digitToInt s) : l)
+                                        | s == '.'  = buildRowHelper ss (Nothing : l)
                                         | s == '\n' = reverse l
-                buildRowHelper _ l                 = reverse la
+                buildRowHelper _ l                  = reverse l
 
 
 ------------------------------------------------------------------------------
@@ -149,8 +124,8 @@ parseSudokuFile s = Sudoku $ map buildRow s
 -- * C1
 
 -- | cell generates an arbitrary cell in a Sudoku
-cell :: Gen (Cell)
-cell = frequency [(9, elements[Nothing]),(1, rNum)]
+cell :: Gen Cell
+cell = frequency [(9, return Nothing),(1, rNum)]
         where
             rNum = do
                   n <- choose(1, 9)
@@ -168,10 +143,8 @@ instance Arbitrary Sudoku where
  -- hint: get to know the QuickCheck function vectorOf
  
 -- * C3
-
 prop_Sudoku :: Sudoku -> Bool
-prop_Sudoku sud = isSudoku sud
-  -- hint: this definition is simple!
+prop_Sudoku = isSudoku
   
 ------------------------------------------------------------------------------
 
@@ -179,41 +152,41 @@ type Block = [Cell] -- a Row is also a Cell
 
 
 -- * D1
-
+-- | Checks whether a Block is a valid sudoku block.I.e. all blocks have nine cells and no Just Ints are duplicated.
 isOkayBlock :: Block -> Bool
-isOkayBlock b = (and $ map checkElement b) 
+isOkayBlock b = all checkCell b 
               && (length b == 9) 
               && (length justs == length (nub justs))
             where
-              justs = filter (/= Nothing) b
+              justs = filter (isJust) b
 
 
 -- * D2
 
+-- | Create all blocks blocks from the sudoku
 blocks :: Sudoku -> [Block]
 blocks (Sudoku sudoku) = cSquares ++ cRows ++ cColumns
       where
         cSquares = getSquares sudoku
         getSquares [] = []
-        getSquares (r1:r2:r3:rs) =  [(take 3 r1) ++ (take 3 r2) ++ (take 3 r3)
-                               , (take 3 (drop 3 r1)) ++ (take 3 (drop 3 r2))  ++ (take 3 (drop 3 r3)) 
-                               , (drop 6 r1) ++ (drop 6 r2) ++ (drop 6 r3)] 
-                               ++ getSquares rs -- TODO: GÖR DENNA SNYGG (Som skepparn 😉)
+        getSquares (r1:r2:r3:rs) =  [take 3 r1 ++ take 3 r2 ++ take 3 r3
+                               , take 3 (drop 3 r1) ++ take 3 (drop 3 r2)  ++ take 3 (drop 3 r3)
+                               , drop 6 r1 ++ drop 6 r2 ++ drop 6 r3] 
+                               ++ getSquares rs
         cRows    = sudoku
-        cColumns = getCols 0 sudoku
-        getCols 8 sud = map (!! 8) sud:[]
-        getCols n sud = map (!! n) sud: getCols (n+1) sud 
+        cColumns = transpose sudoku
 
+-- | check that all blocks are of size 9
 prop_blocks_lengths :: Sudoku -> Bool
-prop_blocks_lengths sudoku = ((length bs) == 27)
-                           && (and $ map (\x -> length x == 9) bs)
-                           where 
-                            bs = blocks sudoku
+prop_blocks_lengths sudoku = (length bs == 27)
+                          && all (\x -> length x == 9) bs
+                  where 
+                    bs = blocks sudoku
 
 -- * D3
-
+-- | Checks whether a given sudoku satisfies all the rules of Sudoku? 
 isOkay :: Sudoku -> Bool
-isOkay s = (isSudoku s) && (and $ map isOkayBlock $ blocks s) -- 👍
+isOkay s = isSudoku s && all isOkayBlock (blocks s)
 
 
 ---- Part A ends here --------------------------------------------------------
@@ -228,16 +201,25 @@ type Pos = (Int,Int)
 -- * E1
 
 blanks :: Sudoku -> [Pos]
-blanks = undefined
+blanks (Sudoku sudoku) = blanks' (concat sudoku) 0
+    where 
 
---prop_blanks_allBlanks :: ...
---prop_blanks_allBlanks =
+      blanks' [] _         = []
+      blanks' (c:concSudoku) i | c == Nothing = 
+                (i `div` 9, i `mod` 9) : blanks' concSudoku (i+1)
+                             |   otherwise = blanks' concSudoku (i+1)
+
+prop_blanks_allBlanks :: Bool
+prop_blanks_allBlanks = (length $ blanks allBlankSudoku) == 81
 
 
 -- * E2
 
 (!!=) :: [a] -> (Int,a) -> [a]
-xs !!= (i,y) = undefined
+xs !!= (i,y) = putaHelpo [] xs (i, y)
+  where 
+    putaHelpo rest (x:xs) (0, a) = rest ++ a ++ xs
+    putaHelpo rest (x:xs) (n, a) = putaHelpo (rest ++ [x]) xs (n-1, a)
 
 --prop_bangBangEquals_correct :: ...
 --prop_bangBangEquals_correct =
