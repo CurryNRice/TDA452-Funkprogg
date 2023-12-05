4ace8ef92fb5e050df1205de459630b40412a418
Karl Kjellmer <karl.kjellmer@gmail.com>
2023-12-05 18:51:47 +0100

    GABBADABBAAAGOOOOOOOOOOOOO

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -3,6 +3,9 @@ module Expr where
   
 import Prelude hiding (sin, cos)
 import qualified Prelude as P (sin, cos)
+import Test.QuickCheck
+
+import Parsing
 
 
 {-data Expr
@@ -65,9 +68,9 @@ size _ = 0
 
 
 showExpr :: Expr -> String
-showExpr (Opr Mul X (Num n)) = showExpr (Num n) ++ "x"
-showExpr (Opr Mul (Num n) X) = showExpr (Num n) ++ "x"
 
+showExpr (Opr Mul (Num (-1)) e) = "-" ++ showFactor e
+showExpr (Opr Mul e (Num (-1))) = "-" ++ showFactor e
 showExpr (Opr Mul e1 e2) = showFactor e1 ++ "*" ++ showFactor e2
 showExpr (Opr Add e1 e2) = showExpr e1 ++ "+" ++ showExpr e2
 showExpr (Func f e) = case f of Sin -> "sin"; Cos -> "cos" 
@@ -88,4 +91,139 @@ eval (Opr Mul e1 e2) x = (eval e1 x) * (eval e2 x)
 eval (Func Sin e1)   x = P.sin $ eval e1 x
 eval (Func Cos e1)   x = P.cos $ eval e1 x
 
+-- | that, given a string, tries to interpret the string as an expression
+-- | and returns Just of that expression if it succeeds.
+-- | Otherwise, Nothing will be returned
+readExpr :: String -> Maybe Expr
+readExpr s = case parse expr s'' of 
+    Just(e, _) -> Just e
+    Nothing -> Nothing
+  where
+    s' = filter (/=' ') s
+    s'' = repl s'
+    repl [] = ""
+    repl (s:'-':ss) | s /= 'e' = s : "(-1)*" ++ (repl ss)
+    repl (s:ss) = s: (repl ss)
 
+
+
+
+expr, term, factor :: Parser Expr
+expr   = foldl1 add <$> chain term (char '+')
+term = foldl1 mul <$> chain factor (char '*')
+factor = Num <$> number' 
+      <|> xParse 
+      <|> trig "sin"
+      <|> trig "cos"
+      <|> char '(' *> expr <* char ')' 
+
+
+trig s = (case s of "sin" -> sin; "cos" -> cos) 
+        <$> (string s *> factor)
+  where 
+    -- | This is UGLY there must be a better way to do it :(
+    string :: String -> Parser String
+    string (s1:s2:s3:ss)=  
+      do 
+        char s1
+        char s2
+        char s3 
+        return (s1:s2:s3:[])
+
+
+xParse :: Parser Expr
+xParse = do 
+  char 'x' 
+  return X
+
+number' = number'' <|> number
+
+number'' = do 
+  char '-'
+  num <- number
+  return (-num)
+
+number :: Parser Double
+number = (parseDouble <|> parseInt) 
+
+parseInt :: Parser Double
+parseInt =  read <$> oneOrMore digit
+
+parseDouble :: Parser Double
+parseDouble = do
+    whole <- oneOrMore digit
+    char '.'
+    fract <- oneOrMore digit
+    sien <- zeroOrMore parseSient
+    pure $ read $ whole <> "." <> fract <> (if sien /= [] then head sien else "")
+
+parseSient = do
+  e <- char 'e'
+  neg <- zeroOrMore $ char '-'
+  n <- oneOrMore digit
+  pure $ "e" <> neg <> n
+
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr e = abs (eval e 5 - eval rwe 5) <= 0.0000001 
+  where (Just rwe) = readExpr $ showExpr e
+
+
+arbExpr :: Int -> Gen Expr
+arbExpr s = frequency [(1, (return X)), (1, rNum), 
+                          (s, rBin), (s, rTrig)]
+      where
+        range = 5
+        rNum  = Num <$> choose(-range,range)
+        rBin  = do
+          op <- elements [Add,Mul]
+          let s' = s `div` 2
+          e1 <- arbExpr s'
+          e2 <- arbExpr s'
+          return $ Opr op e1 e2 
+        rTrig = do
+          tri <- elements [Cos, Sin]
+          let s' = s `div` 2
+          e <- arbExpr s'
+          return $ Func tri e 
+        
+instance Arbitrary Expr where 
+  arbitrary = sized arbExpr
+
+
+
+-- | simplify ; makes you simp, as simple as that
+simplify e  | e == simplify' e = e 
+             | otherwise = simplify $ simplify' e   
+
+simplify' :: Expr -> Expr
+simplify' (Opr Mul (Num 0.0) _) = Num 0.0 
+simplify' (Opr Mul _ (Num 0.0)) = Num 0.0 
+simplify' (Opr Add (Num 0.0) e) = simplify' e
+simplify' (Opr Add e (Num 0.0)) = simplify' e
+simplify' (Opr Add (Num n1) (Num n2)) = Num (n1 + n2)
+simplify' (Opr Add X X) = Opr Mul (Num 2) X
+simplify' (Opr Add (Opr Mul (Num n) X) X) = mul (Num (n+1)) X
+simplify' (Opr Add X (Opr Mul (Num n) X)) = mul (Num (n+1)) X
+simplify' (Opr Add e1 e2) = add (simplify' e1) (simplify' e2)
+simplify' (Opr Mul e1 e2) = mul (simplify' e1) (simplify' e2)
+simplify' (Func Sin e) = Func Sin (simplify' e)
+simplify' (Func Cos e) = Func Cos (simplify' e)
+simplify' (Num n) = Num n
+simplify' X = X
+prop_evalsimplify e n = (eval e n) == (eval (simplify e) n)
+
+
+-- | which differentiates the expression (with respect to x). You should use the simplify function to simplify the result.
+differentiate :: Expr -> Expr
+differentiate e = simplify $ differentiate' e
+  where
+    differentiate' (Num n)             = Num 0.0
+    differentiate' (X)                 = Num 1
+    differentiate' (Opr Mul (Num n) X) = Num n
+    differentiate' (Opr Mul X (Num n)) = Num n
+    differentiate' (Opr Mul X X)       = mul (Num 2) X
+    differentiate' (Func Sin e)        = cos e
+    differentiate' (Func Cos e)        = mul (Num (-1)) (sin e)
+    differentiate' (Opr Add e1 e2)     = add (differentiate' e1) (differentiate' e2)
+    differentiate' (Opr Mul e1 e2)     = add (mul (differentiate' e1) e2) 
+                                        (mul e1 (differentiate' e2))
